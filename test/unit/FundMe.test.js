384397a816a024b3ab9fcdeaa89aee93a1679a24
Sravan T P <sravantp03@gmail.com>
2023-02-11 19:10:41 +0530

    tested constructor & fund function

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,11 @@
 const { deployments, getNamedAccounts, ethers, network } = require("hardhat")
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 
 describe("FundMe", function () {
     let fundMe
     let mockV3Aggregator
     let deployer
+    const sendValue = ethers.utils.parseEther("1") // 1000000000000000000 wei
 
     beforeEach(async () => {
         deployer = (await getNamedAccounts()).deployer
@@ -20,6 +21,29 @@ describe("FundMe", function () {
         it("sets the aggregator addresses correctly", async function () {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+
+        it("deployer and owner address must be same", async function () {
+            const owner = await fundMe.i_owner()
+            assert.equal(deployer, owner)
+        })
+    })
+
+    describe("fund", function () {
+        it("Should fail if don't send enough ETH", async function () {
+            await expect(fundMe.fund()).to.be.reverted
+        })
+
+        it("should update amount funded data structure", async function () {
+            await fundMe.fund({ value: sendValue }) // funding contract
+            const response = await fundMe.addressToAmount(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+
+        it("add funders to array of funders", async function () {
+            await fundMe.fund({ value: sendValue })
+            const funderAddress = await fundMe.funders(0)
+            assert.equal(funderAddress, deployer)
         })
     })
 })
