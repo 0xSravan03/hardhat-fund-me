a4e043cc80fbb42f3cf9a1584dade68c30fb0eb0
Sravan T P <sravantp03@gmail.com>
2023-02-12 14:16:25 +0530

    created more test for fund and withdraw function

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -46,4 +46,101 @@ describe("FundMe", function () {
             assert.equal(funderAddress, deployer)
         })
     })
+
+    describe("withdraw", function () {
+        beforeEach(async function () {
+            await fundMe.fund({ value: ethers.utils.parseEther("2") })
+        })
+
+        it("Owner should able to withdraw ETH from a single funder", async function () {
+            const startingFundMeBalance = await ethers.provider.getBalance(
+                fundMe.address
+            )
+
+            const startingDeployerBalance = await ethers.provider.getBalance(
+                deployer
+            )
+
+            const tx = await fundMe.withdraw() // cost gas from deployer
+            const txReceipt = await tx.wait(1)
+
+            const endingFundMeBalance = await ethers.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await ethers.provider.getBalance(
+                deployer
+            )
+
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            // gasCost = gasUsed * effectiveGasPrice ()
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            assert.equal(endingFundMeBalance, "0")
+            assert.equal(
+                startingDeployerBalance.add(startingFundMeBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+        })
+
+        it("allow owner to withdraw fund if there are multiple funders", async function () {
+            const accounts = await ethers.getSigners() // hardhat helper function that retrive accounts
+
+            for (let i = 1; i <= 5; i++) {
+                const newContractInstance = await fundMe.connect(accounts[i]) // connecting each account with fundMe contract
+                await newContractInstance.fund({
+                    value: ethers.utils.parseEther(i.toString()),
+                }) // funding contract
+            }
+
+            const startingFundMeBalance = await ethers.provider.getBalance(
+                fundMe.address
+            )
+
+            const startingDeployerBalance = await ethers.provider.getBalance(
+                deployer
+            )
+
+            const tx = await fundMe.withdraw()
+            const txReceipt = await tx.wait(1)
+
+            const endingFundMeBalance = await ethers.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await ethers.provider.getBalance(
+                deployer
+            )
+
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            // gasCost = gasUsed * effectiveGasPrice ()
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            assert.equal(endingFundMeBalance, "0")
+            assert.equal(
+                startingDeployerBalance.add(startingFundMeBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            await expect(fundMe.funders(0)).to.be.reverted // checking if the funders array is resetted after withdrawing amount
+
+            // checking if all funded address is mapping to 0 after withdrawal
+            for (let i = 0; i <= 5; i++) {
+                assert.equal(
+                    await fundMe.addressToAmount(accounts[i].address),
+                    0
+                )
+            }
+        })
+
+        it("only allow the owner to withdraw amount", async function () {
+            const [, unknownUser] = await ethers.getSigners() // taking second account from hardhat n/w because first account is our deployer
+            const contractInstance = await fundMe.connect(unknownUser) // connecting account to contract
+
+            await expect(
+                contractInstance.withdraw()
+            ).to.be.to.be.revertedWithCustomError(
+                contractInstance,
+                "FundMe__NotOwner"
+            ) // reverting with custom error
+        })
+    })
 })
